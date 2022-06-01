4846bdf9fd9e77ffff2e1e6b7fe258029403fae9
Maria <sheyko.mari@mail.ru>
2022-06-01 22:34:30 +0300

    refactor: remove tag selectors according to rule 3.4 in in html-and-css.md

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton=document.querySelectorAll(".btn")[0];//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -82,7 +82,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector('.section__input');
     var label=listItem.querySelector("p");
     var editBtn=listItem.querySelector(".btn_edit");
     var containsClass=listItem.classList.contains("list__item_edit");
@@ -155,7 +155,7 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
+    var checkBox=taskListItem.querySelector(".checkbox");
     var editButton=taskListItem.querySelector(".btn_edit");
     var deleteButton=taskListItem.querySelector(".btn_delete");
 
